8bc0887728a59fe6a52ea0eb9089110c3ade107c
Ashleyjeffers2002 <alencia.j.student@lyonscommunityschool.org>
2019-10-24 22:28:34 +0000

    comment goes here

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
-
-
-  
-  
-  
-  
+    $("#button").click(function() {
+        let Userinput = $(".input").val();
+        console.log(Userinput)
+    })
 });
